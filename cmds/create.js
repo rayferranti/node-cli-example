4cfabf83cb5c43364131fce65beb0493f138ebe0
ray <ray@rays-MBP.home>
2018-07-10 21:04:13 -0400

    path to the new folder is now required

--- cmds/create.js
+++ cmds/create.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+
 module.exports = (args) => {
+  const {
+    component,
+    path,
+  } = args;
+  if (!component) {
+    console.error('You must specify a component name.');
+    process.exit(1);
+  }
+  if (!path) {
+    console.error('You must specify a path for the component');
+    console.log(process.cwd());
+    process.exit(1);
+  }
   console.log('CREATE THIS FOLDER');
+  console.log(args);
 }
