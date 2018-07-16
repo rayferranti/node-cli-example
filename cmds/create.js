6cebb3e4a8d8187356ab3c98896b399e81edb292
ray <ray@rays-MacBook-Pro.local>
2018-07-16 16:13:41 -0400

    check to see if the folder with the given name already exist

--- cmds/create.js
+++ cmds/create.js
@@ -5,15 +5,25 @@ module.exports = (args) => {
     component,
     path,
   } = args;
+
   if (!component) {
     console.error('You must specify a component name.');
     process.exit(1);
   }
+
   if (!path) {
-    console.error('You must specify a path for the component');
+    console.error('You must specify a path for the component.');
+    console.log(process.cwd());
+    process.exit(1);
+  }
+
+  if (fs.statSync(path).isDirectory()) {
+    console.error(`A folder with the name ${component} at the location ${path} already exist.`);
     console.log(process.cwd());
     process.exit(1);
   }
   console.log('CREATE THIS FOLDER');
-  console.log(args);
+  console.log(component);
+  console.log('AT THIS LOCATION');
+  console.log(path);
 }
